7c940d637940c45c8dd335fade5ba4588713ab47
developer@trashcash.ph <sacdalanryanchristian6404@gmail.com>
2021-12-27 18:14:29 +0800

    Initial project structure

---
+++ src/Components/BookList.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class BookList extends React.Component {
+  render() {
+    return (
+      <div className="book-list">
+        <h1>Book names</h1>
+        <ul>
+          <li>The name of the Kings</li>
+          <li>King Speech</li>
+          <li>The Final Empire</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default BookList;
