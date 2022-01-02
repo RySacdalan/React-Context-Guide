bbc6ee8b807bcc810a73a2587a8ba2f803c502bd
developer@trashcash.ph <sacdalanryanchristian6404@gmail.com>
2022-01-02 21:19:30 +0800

    Accessing/consuming the context.

--- src/Components/BookList.js
+++ src/Components/BookList.js
@@ -1,14 +1,24 @@
 import React, { Component } from "react";
+import { ThemeContext } from "../Context/ThemeContext";
 
 class BookList extends Component {
+  static contextType = ThemeContext;
+
   render() {
+    const { isLightTheme, light, dark } = this.context;
+    //ternary rendering
+    const theme = isLightTheme ? light : dark;
+
     return (
-      <div className="book-list">
+      <div
+        className="book-list"
+        style={{ background: theme.bg, color: theme.syntax }}
+      >
         <h1>Book names</h1>
         <ul>
-          <li>The name of the Kings</li>
-          <li>King Speech</li>
-          <li>The Final Empire</li>
+          <li style={{ background: theme.ui }}>The name of the Kings</li>
+          <li style={{ background: theme.ui }}>King Speech</li>
+          <li style={{ background: theme.ui }}>The Final Empire</li>
         </ul>
       </div>
     );
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,9 +1,18 @@
 import React, { Component } from "react";
+import { ThemeContext } from "../Context/ThemeContext";
 
 class Navbar extends Component {
+  //Use to transfer data from the context/provider.
+  static contextType = ThemeContext;
   render() {
+    console.log(this.context);
+    const { isLightTheme, light, dark } = this.context;
+    //ternary rendering
+    const theme = isLightTheme ? light : dark;
+
+    //put all jscode above this return() code block.
     return (
-      <nav>
+      <nav style={{ background: theme.ui, color: theme.syntax }}>
         <h1>ContextAPI</h1>
         <ul>
           <li>Home</li>
