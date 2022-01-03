8252a7e211505e56dc04bfb1c848d0cfc101417b
developer@trashcash.ph <sacdalanryanchristian6404@gmail.com>
2022-01-03 08:54:13 +0800

    Consuming multiple context in single component.

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,25 +1,40 @@
 import React, { Component } from "react";
+import { AuthContext } from "../Context/AuthContext";
 import { ThemeContext } from "../Context/ThemeContext";
 
 class Navbar extends Component {
-  //Use to transfer data from the context/provider.
-  static contextType = ThemeContext;
   render() {
     console.log(this.context);
-    const { isLightTheme, light, dark } = this.context;
-    //ternary rendering
-    const theme = isLightTheme ? light : dark;
 
-    //put all jscode above this return() code block.
     return (
-      <nav style={{ background: theme.ui, color: theme.syntax }}>
-        <h1>ContextAPI</h1>
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Contact</li>
-        </ul>
-      </nav>
+      //consuming two context in one component
+      <AuthContext.Consumer>
+        {(authContext) => (
+          <ThemeContext.Consumer>
+            {(themeContext) => {
+              //destructuring the passed data/value
+              const { isAuthenticated, toggleAuth } = authContext;
+              const { isLightTheme, light, dark } = themeContext;
+              const theme = isLightTheme ? light : dark;
+
+              //returning navbar jsx after context
+              return (
+                <nav style={{ background: theme.ui, color: theme.syntax }}>
+                  <h1>ContextAPI</h1>
+                  <div onClick={toggleAuth}>
+                    {isAuthenticated ? "Logged in" : "Logged out"}
+                  </div>
+                  <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                  </ul>
+                </nav>
+              );
+            }}
+          </ThemeContext.Consumer>
+        )}
+      </AuthContext.Consumer>
     );
   }
 }
